a7f9826426105cc6ddca88c88ca62f03995a7570
Rajadhi Wickramatunga <rajadhi.wickramatunga@oracle.com>
2021-06-09 20:31:04 +1000

    Basic implementation

--- src/main/java/com/rajadhi/code/WordPermutation.java
+++ src/main/java/com/rajadhi/code/WordPermutation.java
@@ -1,9 +1,36 @@
-package java.com.rajadhi.code;
+package com.rajadhi.code;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class WordPermutation {
+
+    public Set<String> init(String input) {
+        return getPermutations(input);
+    }
+
+
+    //Ab
     public Set<String> getPermutations(String input) {
-        return null;
+        Set<String> output = new HashSet<>();
+
+        if (input.length() == 1) {
+            if (Character.isLowerCase(input.charAt(0))) {
+                output.add("" + Character.toUpperCase(input.charAt(0)));
+            }
+            output.add("" + input.charAt(0));
+        } else {
+
+            Set<String> subsequentPermutations = getPermutations(input.substring(1));
+
+            for (String s : subsequentPermutations) {
+                if (Character.isLowerCase(input.charAt(0))) {
+                    output.add("" + Character.toUpperCase(input.charAt(0)) + s);
+                }
+                output.add("" + input.charAt(0) + s);
+            }
+
+        }
+        return output;
     }
 }
--- src/test/java/com/rajadhi/code/WordPermutationTest.java
+++ src/test/java/com/rajadhi/code/WordPermutationTest.java
@@ -1,19 +1,29 @@
-package java.com.rajadhi.code;
+package com.rajadhi.code;
 
 import static java.util.Arrays.asList;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 import java.util.HashSet;
 import java.util.Set;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-class WordPermutationTest {
+
+public class WordPermutationTest {
+
+    @Test
+    public void getPermutations_shouldReturn_allPermutationsForOneLetters() {
+        WordPermutation wordPermutation = new WordPermutation();
+        Set<String> permutations = wordPermutation.init("a");
+
+        Set<String> expected = new HashSet<>(asList("a", "A"));
+        assertEquals(expected, permutations);
+    }
 
     @Test
     public void getPermutations_shouldReturn_allPermutationsForTwoLetters() {
         WordPermutation wordPermutation = new WordPermutation();
-        Set<String> permutations = wordPermutation.getPermutations("Ab");
+        Set<String> permutations = wordPermutation.init("Ab");
 
         Set<String> expected = new HashSet<>(asList("Ab", "AB"));
         assertEquals(expected, permutations);
@@ -22,18 +32,28 @@ class WordPermutationTest {
     @Test
     public void getPermutations_shouldReturn_allPermutationsForThreeLetters() {
         WordPermutation wordPermutation = new WordPermutation();
-        Set<String> permutations = wordPermutation.getPermutations("abC");
+        Set<String> permutations = wordPermutation.init("abC");
 
-        Set<String> expected = new HashSet<>(asList("AbC", "aBC", "ABC"));
+        Set<String> expected = new HashSet<>(asList("abC", "AbC", "aBC", "ABC"));
         assertEquals(expected, permutations);
     }
 
     @Test
     public void getPermutations_shouldReturn_allPermutationsForFourLetters() {
         WordPermutation wordPermutation = new WordPermutation();
-        Set<String> permutations = wordPermutation.getPermutations("AbcD");
+        Set<String> permutations = wordPermutation.init("AbcD");
 
         Set<String> expected = new HashSet<>(asList("AbcD", "ABcD", "AbCD", "ABCD"));
+        assertEquals(expected, permutations);
+    }
+
+
+    @Test
+    public void getPermutations_shouldReturn_allPermutationsForFiveLetters() {
+        WordPermutation wordPermutation = new WordPermutation();
+        Set<String> permutations = wordPermutation.init("AbcDe");
+
+        Set<String> expected = new HashSet<>(asList("ABcDE", "ABcDe", "AbcDE", "AbcDe", "ABCDE", "ABCDe", "AbCDE", "AbCDe"));
         assertEquals(expected, permutations);
     }
 }
